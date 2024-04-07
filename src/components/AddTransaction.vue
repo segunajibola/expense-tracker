991a43f6c4cff09449fd0afdfc6c49d0a22341dc
Segun Ajibola <ajbl.segun@gmail.com>
2024-04-07 09:23:46 +0100

    add minus tailwindcss

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -2,8 +2,8 @@
   <h3 class="border-b-2 border-style-solid border-[#bbb] pb-[10px] mt-[40px] mx-0 mb-[10px]">Add new transaction</h3>
   <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
-      <label for="text" class="my-2 inline-block"Text</label>
-      <input type="text" id="text" placeholder="Enter text..." v-model="text" />
+      <label for="text" class="my-2 inline-block">Text</label>
+      <input type="text" id="text" placeholder="Enter text..." v-model="text" class="border border-solid border-[#dedede] rounded-[2px] block text-[16px] p-2.5 w-full"/>
     </div>
     <div class="form-control">
       <label for="amount" class="my-2 inline-block"
@@ -15,9 +15,9 @@
         id="amount"
         placeholder="Enter amount..."
         v-model="amount"
-      />
+      class="border border-solid border-[#dedede] rounded-[2px] block text-[16px] p-2.5 w-full focus:outline-0"/>
     </div>
-    <button class="btn">Add transaction</button>
+    <button class="btn cursor-pointer bg-[#9c88ff] text-[#fff] border-none block text-[16px] mt-2.5 mb-7.5 p-2.5 w-full">Add transaction</button>
   </form>
 </template>
 
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,13 +1,24 @@
 <template>
-  <h3 class="border-b-2 border-style-solid border-[#bbb] pb-[10px] mt-[40px] mx-0 mb-[10px]">History</h3>
-  <ul id="list" class="list">
+  <h3
+    class="border-b-2 border-style-solid border-[#bbb] pb-[10px] mt-[40px] mx-0 mb-[10px]"
+  >
+    History
+  </h3>
+  <ul id="list" class="list-none p-0 mb-">
     <li
       v-for="transaction in transactions"
       :key="transaction.id"
-      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+      :class="{
+        'flex bg-[#fff] text-[#333] justify-between relative p-2.5 my-2.5 border-r-[5px] border-solid': true,
+        'border-[#c0392b]': transaction.amount < 0,
+        'border-[#2ecc71]': transaction.amount >= 0,
+      }"
     >
       {{ transaction.text }} <span>${{ transaction.amount }}</span
-      ><button class="delete-btn" @click="deleteTransaction(transaction.id)">
+      ><button
+        class="delete-btn focus:outline-0"
+        @click="deleteTransaction(transaction.id)"
+      >
         x
       </button>
     </li>
@@ -15,7 +26,7 @@
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps } from "vue";
 
 const props = defineProps({
   transactions: {
@@ -24,9 +35,9 @@ const props = defineProps({
   },
 });
 
-const emit = defineEmits(['transactionDeleted']);
+const emit = defineEmits(["transactionDeleted"]);
 
 const deleteTransaction = (id) => {
-  emit('transactionDeleted', id);
+  emit("transactionDeleted", id);
 };
 </script>
